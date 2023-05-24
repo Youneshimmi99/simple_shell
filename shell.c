e6506aba97ac7aa23c933b0d3c59a0088a847710
Youneshimmi99 <himmiyounes99@gmail.com>
2023-05-25 00:31:56 +0100

    fix shell.c

--- shell.c
+++ shell.c
@@ -1,54 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+#define BUFSIZE 1024
 
 /**
- * main - Shell program that read user input and executes commands.
+ * main - Entry point for simple shell
  *
- * Return: Always 0.
+ * Return: Always 0
  */
 int main(void)
 {
-    char *line = NULL;
-    size_t line_size = 0;
+    char buffer[BUFSIZE];
 
     while (1)
     {
         printf("#cisfun$ ");
 
-        if (getline(&line, &line_size, stdin) < 0)
-            break;
-
-        line[strcspn(line, "\n")] = '\0';
-
-        if (access(line, X_OK) == 0)
+        if (fgets(buffer, BUFSIZE, stdin) == NULL)
         {
-            pid_t pid = fork();
-            if (pid == 0)
+            if (feof(stdin))
             {
-                char *args[] = {line, NULL};
-                execve(line, args, environ);
-                fprintf(stderr, "Error: failed to execute command '%s'\n", line);
-                exit(EXIT_FAILURE);
-            }
-            else if (pid < 0)
-            {
-                fprintf(stderr, "Error: failed to fork process\n");
-                exit(EXIT_FAILURE);
+                printf("\n");
+                exit(EXIT_SUCCESS);
             }
             else
             {
-                wait(NULL);
+                perror("simple_shell");
+                exit(EXIT_FAILURE);
             }
+        }
+
+        buffer[strcspn(buffer, "\n")] = 0;
+
+        pid_t pid;
+        pid = fork();
+
+        if (pid < 0)
+        {
+            perror("simple_shell");
+        }
+        else if (pid == 0)
+        {
+            if (execve(buffer, NULL, environ) == -1)
+            {
+                perror("simple_shell");
+            }
+            exit(EXIT_FAILURE);
         }
         else
         {
-            fprintf(stderr, "Error: command '%s' not found\n", line);
+            wait(NULL);
         }
     }
 
-    free(line);
-
-    return 0;
+    return (0);
 }
